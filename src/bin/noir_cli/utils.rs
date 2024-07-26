258ba21cbb71619753ecbbfea9442a7c890e3738
tarassh <tshchybo@gmail.com>
2024-07-26 07:45:50 -0700

    use thiserror for cli

--- src/bin/noir_cli/utils.rs
+++ src/bin/noir_cli/utils.rs
@@ -13,17 +13,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use anyhow::{Context, Result};
+use crate::errors::CliError;
 use regex::Regex;
 use std::fs::File;
 use std::io::Write;
 
-pub fn out_file(output: Option<&std::path::PathBuf>) -> Result<Box<dyn std::io::Write>> {
+pub fn out_file(output: Option<&std::path::PathBuf>) -> Result<Box<dyn std::io::Write>, CliError> {
     // Attempt to create the file if a path is specified
     let from_path = output
         .map(|p| {
             // Try to create the file and add context to any error that occurs
-            File::create(&p).with_context(|| format!("Failed to create output file {:?}", &p))
+            File::create(&p)
+                .map_err(|_| CliError::CliError(format!("Failed to create output file {:?}", &p)))
         })
         .transpose()? // Convert Option<Result<File>> to Result<Option<File>>
         .map(|f| Box::new(f) as Box<dyn Write>); // Box the file writer
@@ -37,7 +38,7 @@ pub fn encode_value_as_u32(
     variable_part: &str,
     buf: &mut [u8],
     offset: &mut usize,
-) -> Result<()> {
+) -> Result<(), CliError> {
     encode_value(value, variable_part, buf, offset, 4)
 }
 
@@ -46,11 +47,11 @@ pub fn encode_value_as_u256(
     variable_part: &str,
     buf: &mut [u8],
     offset: &mut usize,
-) -> Result<()> {
+) -> Result<(), CliError> {
     encode_value(value, variable_part, buf, offset, 32)
 }
 
-pub fn encode_str(key: &str, buf: &mut [u8], offset: &mut usize) -> Result<()> {
+pub fn encode_str(key: &str, buf: &mut [u8], offset: &mut usize) -> Result<(), CliError> {
     encode_u32(key.len() as u32, buf, offset);
 
     buf[*offset..*offset + key.len()].copy_from_slice(key.as_bytes());
@@ -69,43 +70,54 @@ pub fn encode_value(
     buf: &mut [u8],
     offset: &mut usize,
     length: usize,
-) -> Result<()> {
+) -> Result<(), CliError> {
     let pattern = format!(
         r"mstore\(add\(_vk, {}\), (0x[0-9a-fA-F]{{64}})\)",
         regex::escape(variable_part)
     );
 
-    let re = Regex::new(&pattern)?;
+    let re = Regex::new(&pattern).map_err(|_| {
+        CliError::CliError(format!("Failed to compile regex pattern: {:?}", pattern))
+    })?;
 
     if let Some(cap) = re.captures(value) {
         let hex_value = &cap[1];
-        let decoded_value = hex::decode(hex_value.strip_prefix("0x").unwrap_or(hex_value))?;
+        let decoded_value = hex::decode(hex_value.strip_prefix("0x").unwrap_or(hex_value))
+            .map_err(|_| {
+                CliError::CliError(format!("Failed to decode hex string: {:?}", hex_value))
+            })?;
         let decoded_value = &decoded_value[32 - length..];
 
         if buf.len() < *offset + decoded_value.len() {
-            return Err(anyhow::anyhow!("Buffer is too small"));
+            return Err(CliError::CliError(format!(
+                "Buffer is too small: {} < {}",
+                buf.len(),
+                *offset + decoded_value.len()
+            )));
         }
 
         buf[*offset..*offset + decoded_value.len()].copy_from_slice(&decoded_value);
         *offset += decoded_value.len();
         Ok(())
     } else {
-        Err(anyhow::anyhow!(
+        Err(CliError::CliError(format!(
             "Failed to extract value from: {:?}",
             variable_part
-        ))
+        )))
     }
 }
 
-pub fn encode_hex(hex_str: &str, buf: &mut Vec<u8>) -> Result<()> {
-    let decoded_value = hex::decode(hex_str.strip_prefix("0x").unwrap_or(hex_str))?;
+pub fn encode_hex(hex_str: &str, buf: &mut Vec<u8>) -> Result<(), CliError> {
+    let decoded_value = hex::decode(hex_str.strip_prefix("0x").unwrap_or(hex_str))
+        .map_err(|_| CliError::CliError(format!("Failed to decode hex string: {:?}", hex_str)))?;
     buf.extend_from_slice(&decoded_value);
     Ok(())
 }
 
-pub fn encode_pub_inputs(pub_inputs: &[String], buf: &mut Vec<u8>) -> Result<()> {
+pub fn encode_pub_inputs(pub_inputs: &[String], buf: &mut Vec<u8>) -> Result<(), CliError> {
     for input in pub_inputs {
-        let decoded_value = hex::decode(input.strip_prefix("0x").unwrap_or(input))?;
+        let decoded_value = hex::decode(input.strip_prefix("0x").unwrap_or(input))
+            .map_err(|_| CliError::CliError(format!("Failed to decode hex string: {:?}", input)))?;
         buf.extend_from_slice(&decoded_value);
     }
     Ok(())
--- src/bin/noir_cli/verifier.rs
+++ src/bin/noir_cli/verifier.rs
@@ -13,22 +13,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::path::PathBuf;
 use ultraplonk_verifier::{verify as verify_proof, Proof, PublicInput, VerificationKey};
 
-use anyhow::{Context, Result};
-use std::path::PathBuf;
-
 use crate::cli::Commands;
+use crate::errors::CliError;
 
-pub fn process_command(command: &Commands, verbose: bool) -> Result<()> {
+pub fn process_command(command: &Commands, verbose: bool) -> Result<(), CliError> {
     if let Commands::Verify { proof, pubs, key } = command {
         verify(key, proof, pubs, verbose)
     } else {
-        Err(anyhow::anyhow!("Invalid command for verifier"))
+        Err(CliError::CliError("Invalid command".to_string()))
     }
 }
 
-fn verify(key: &PathBuf, proof: &PathBuf, pubs: &PathBuf, verbose: bool) -> Result<()> {
+fn verify(key: &PathBuf, proof: &PathBuf, pubs: &PathBuf, verbose: bool) -> Result<(), CliError> {
     if verbose {
         println!("Reading key file: {:?}", key);
         println!("Reading proof file: {:?}", proof);
@@ -36,19 +35,23 @@ fn verify(key: &PathBuf, proof: &PathBuf, pubs: &PathBuf, verbose: bool) -> Resu
     }
 
     // Read and process the proof file
-    let key_data =
-        std::fs::read(key).with_context(|| format!("Failed to read proof file: {:?}", key))?;
+    let key_data = std::fs::read(key)
+        .map_err(|_| CliError::CliError(format!("Failed to read proof file: {:?}", key)))?;
 
-    let vk = VerificationKey::try_from(&key_data[..])
-        .with_context(|| format!("Failed to parse verification key from file: {:?}", key))?;
+    let vk = VerificationKey::try_from(&key_data[..]).map_err(|_| {
+        CliError::CliError(format!(
+            "Failed to parse verification key from file: {:?}",
+            key
+        ))
+    })?;
 
     // Read and process the proof file
     let proof = read_proof_file(proof)
-        .with_context(|| format!("Failed to read proof file: {:?}", proof))?;
+        .map_err(|_| CliError::CliError(format!("Failed to read proof file: {:?}", proof)))?;
 
     // Read and process the key file
-    let pubs =
-        std::fs::read(pubs).with_context(|| format!("Failed to read key file: {:?}", pubs))?;
+    let pubs = std::fs::read(pubs)
+        .map_err(|_| CliError::CliError(format!("Failed to read key file: {:?}", pubs)))?;
 
     // Convert input data into a slice of [PublicInput]
     let pubs = convert_to_pub_inputs(&pubs)?;
@@ -62,16 +65,19 @@ fn verify(key: &PathBuf, proof: &PathBuf, pubs: &PathBuf, verbose: bool) -> Resu
             println!("Proof is valid");
             Ok(())
         }
-        Err(e) => {
-            println!("Verification failed with error: {:?}", e);
-            Err(anyhow::anyhow!("Proof is invalid"))
-        }
+        Err(e) => Err(CliError::CliError(format!(
+            "Verification failed with error: {:?}",
+            e
+        ))),
     }
 }
 
-fn convert_to_pub_inputs(data: &[u8]) -> Result<&[PublicInput]> {
+fn convert_to_pub_inputs(data: &[u8]) -> Result<&[PublicInput], CliError> {
     if data.len() % 32 != 0 {
-        return Err(anyhow::anyhow!("Data length is not a multiple of 32"));
+        return Err(CliError::CliError(format!(
+            "Invalid public inputs length: {:?}",
+            data.len()
+        )));
     }
 
     let pub_inputs =
@@ -80,11 +86,15 @@ fn convert_to_pub_inputs(data: &[u8]) -> Result<&[PublicInput]> {
     Ok(pub_inputs)
 }
 
-fn read_proof_file(path: &PathBuf) -> Result<Proof> {
-    let data = std::fs::read(path).with_context(|| format!("Failed to read file: {:?}", path))?;
+fn read_proof_file(path: &PathBuf) -> Result<Proof, CliError> {
+    let data = std::fs::read(path)
+        .map_err(|_| CliError::CliError(format!("Failed to read file: {:?}", path)))?;
 
     if data.len() != ultraplonk_verifier::PROOF_SIZE {
-        return Err(anyhow::anyhow!("File size is not 2144 bytes: {:?}", path));
+        return Err(CliError::CliError(format!(
+            "File size is not 2144 bytes: {:?}",
+            ultraplonk_verifier::PROOF_SIZE
+        )));
     }
 
     let mut array = [0u8; ultraplonk_verifier::PROOF_SIZE];
